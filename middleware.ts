8c9ad012f7582ea93be9300b22f9031cd0ef7529
Samaikya Venna <sam@Samaikyas-MacBook-Air.local>
2025-09-15 22:23:12 +0200

    changes

--- middleware.ts
+++ middleware.ts
@@ -9,4 +9,4 @@ export const config = { matcher:
 '/(api|trpc)(.*)', 
 ], 
 };
-export const runtime = 'nodejs'; 
+ 
--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
+  /* config options here */
+  images:{
+    remotePatterns:[
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
       },
     ],
-  },
-  // 👇 Add this section to force Node.js runtime instead of Edge
-  experimental: {
-    runtime: "nodejs",
+
   },
 };
 
