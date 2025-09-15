9bddcc5bf5a376cf1532807b21df8dc4af369dbe
Samaikya Venna <sam@Samaikyas-MacBook-Air.local>
2025-09-15 21:44:15 +0200

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+import { NextResponse } from 'next/server';
+
+export default withClerkMiddleware(() => {
+  // You can add custom logic here if needed
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
+
+function withClerkMiddleware(arg0: () => NextResponse<unknown>) {
+  throw new Error('Function not implemented.');
+}
